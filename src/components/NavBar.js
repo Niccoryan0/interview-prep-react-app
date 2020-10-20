bafece452a97e91c86d7d74f1eb3b890b2c69750
Niccoryan0 <niccoryan0@gmail.com>
2020-10-20 00:01:32 -0700

    styling navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
-import { Nav, NavItem, NavDropdown } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap';
+import { Nav, NavItem, NavDropdown, Button } from 'react-bootstrap'
+import ReactRouterBootstrap, { LinkContainer } from 'react-router-bootstrap';
 
 import { AppBar, Typography, IconButton, Toolbar } from '@material-ui/core';
 
 
 export default class NavBar extends React.Component{
+
+  // refresh(){
+  //   location.reload();
+  // }
+
   render(){
     return(
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Interview Prep</Navbar.Brand>
+        <Navbar.Brand href="/">Interview Prep</Navbar.Brand>
         <Nav className="mr-auto">
-          <LinkContainer href={{pathname:"/Questions", state: {questionType: "technical"}}}><NavItem>Technical</NavItem></LinkContainer>
-          <LinkContainer href={{pathname:"/Questions", state: {questionType: "behavioral"}}}><NavItem>Behavioral</NavItem></LinkContainer>
-          <LinkContainer href={{pathname:"/Questions", state: {questionType: "whiteboard"}}}><NavItem>Whiteboard</NavItem></LinkContainer>
-          <LinkContainer href={{pathname:"/Questions", state: {questionType: "traversal"}}}><NavItem>Traversals</NavItem></LinkContainer>
+          <LinkContainer to={{pathname:"/Questions", state: {questionType: "technical"}}}>
+            <NavItem>
+              <Button>Technical
+                </Button>
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer to={{pathname:"/Questions", state: {questionType: "behavioral"}}}><NavItem>Behavioral</NavItem></LinkContainer>
+          <LinkContainer to={{pathname:"/Questions", state: {questionType: "whiteboard"}}}><NavItem>Whiteboard</NavItem></LinkContainer>
+          <LinkContainer to={{pathname:"/Questions", state: {questionType: "traversals"}}}><NavItem>Traversals</NavItem></LinkContainer>
         </Nav>
         {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
