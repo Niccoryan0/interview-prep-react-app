33303e1622bb3ece446eae6c0a6949a50ca23ea4
Niccoryan0 <niccoryan0@gmail.com>
2020-11-11 20:12:24 -0800

    working on comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -8,9 +8,19 @@ class Comment extends React.Component{
 
   render(){
     return(
-      
+      <div className="comment panel panel-default">
+        <img src={this.props.comment.avatar}></img>
+        <div className="panel-heading">
+          <h4>{this.props.comment.username}</h4>
+        </div>
+        <div className="panel-body">
+          {this.props.comment.comment}
+        </div>
+      </div>
     )
   }
 }
+
+(sortSwitch === "title" ? (a.title > b.title ? 1 : -1) : (a.horns > b.horns ? 1 : -1))
 
 export default Comment;
