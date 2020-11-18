f69bdfd448118d0e448929a191051ab10c812338
Niccoryan0 <niccoryan0@gmail.com>
2020-11-17 20:07:55 -0800

    converting to functions pt2

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import fetchData from './Functions/getData'
 
 
 
-const [allQuestions, setQuestions] = React.useState([]);
-const [question, setQuestion] = React.useState("Retreiving questions, one moment.");
-const [answer, setAnswer] = React.useState("");
-const [category, selectCategory] = React.useState("");
-const [flipped, setFlipped] = React.useState(false);
-const [questionType, setQuestionType] = React.useState(false);
+const [allQuestions, setQuestions] = useState([]);
+const [question, setQuestion] = useState("Retreiving questions, one moment.");
+const [answer, setAnswer] = useState("");
+const [category, setCategory] = useState("");
+const [flipped, setFlipped] = useState(false);
+const [questionType, setQuestionType] = useState(false);
 
 
 const getQuestion = () => {
-    let url = 'https://interviewprepapp.azurewebsites.net/api/' + this.props.location.state.questionType;
-    fetch(url, {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': "*",
-      }
-    }).then(res => res.json())
-    .then(questions => {
-      if(questions){
-        this.shuffleQuestions(questions);
-      }
-      this.getTopQuestion();
-    })
+  const questions = await fetchData(questionType);
+  if(questions){
+    shuffleQuestions(questions);
+  }
+  getTopQuestion();
 }
 
 const shuffleQuestions = (tempArr) => {
@@ -41,64 +31,56 @@ const shuffleQuestions = (tempArr) => {
     tempArr[currentIndex] = tempArr[randomIndex];
     tempArr[randomIndex] = temporaryValue;
   }
-  this.setState((state) => {
-    return {allQuestions : tempArr}
-  })
+  setQuestions(tempArr)
 }
 
 const getTopQuestion = () =>{
-  const current = this.state.allQuestions.pop();
+  const current = allQuestions.pop();
   if(current){
-    this.setState((state) => {
-      return {
-        question : current.question,
-        answer : current.answer,
-        category : current.category,
-        flipped : false
-      }
-    })
-  }
-}
-
-const componentDidMount = () =>{
-  this.getQuestion();
-}
-
-const componentDidUpdate(prevProps, prevState){
-  if (this.props.location.state.questionType !== prevProps.location.state.questionType) {
-    this.getQuestion();
+    setQuestion(current.question);
+    setAnswer(current.answer);
+    setCategory(current.category);
+    setFlipped(false);
   }
 }
 
 const flipCard = () =>{
-  if(this.state.answer){
-    const current = this.state.flipped;
-    this.setState({ flipped: !current });
+  if(answer){
+    setFlipped(!flipped)
   }
 }
   
 const Question = () => {
-  React.useEffect(() => {
-    getQuizzes();
+  useEffect(() => {
+    getQuestion();
   }, []);
+  useEffect(() => {
+    const prev = useRef();
+    useEffect(() => {
+      prev.current = value;
+    });
+    if (questionType !== prev) {
+      getQuestion();
+    }
+  });
   return (
     <div className="body-container">
       <button className="btn btn-dark" onClick={getTopQuestion}>Get New Question</button>
       <div className="container">
           <div className="row">
               <div className="col-md-4 card-container" onClick={flipCard}>
-                  <div className={state.flipped ? "card-flip flip" : "card-flip"}>
+                  <div className={flipped ? "card-flip flip" : "card-flip"}>
                       <div className="card front">
-                        <p className="font-weight-bold blue-text">{state.question}</p>
-                        {state.category &&
-                        <p>Category: {state.category}</p>
+                        <p className="font-weight-bold blue-text">{question}</p>
+                        {category &&
+                        <p>Category: {category}</p>
                         }
-                        {state.answer &&
+                        {answer &&
                         <p className="click-flip-explanation">Click to see answer</p>
                         }
                       </div>
                       <div className="card back">
-                        <p className="font-weight-bold blue-text">{state.answer}</p>
+                        <p className="font-weight-bold blue-text">{answer}</p>
                       </div>
                   </div>
               </div>
