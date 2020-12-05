7d0c3e2a6124c507bfd493ea216bea2bcce695dc
Niccoryan0 <niccoryan0@gmail.com>
2020-12-04 16:25:51 -0800

    working on getting forum to render

--- src/components/Comments/Quizzes.js
+++ src/components/Comments/Quizzes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 
 // const viewQuiz = (rowData) =>{
@@ -6,41 +6,53 @@ import { DataGrid } from '@material-ui/data-grid';
 //   selectQuiz(rowData);
 // }
 
-const getQuizzes = () =>{
-  let url = 'https://interviewprepapp.azurewebsites.net/api/quiz/20';
-  fetch(url, {
+const Quizzes = () =>{
+  const [selectedQuiz, selectQuiz] = useState(null);
+  const [quizzes, setQuizzes] = useState(null);
+  const [rows, setRows] = useState(null);
+  
+  const columns = [
+    { field: 'id', headerName: 'ID', width: '15%' },
+    { field: 'date', headerName: 'Date', width: '30%' },
+    { field: 'username', headerName: 'Username', width: '30%' },
+    { field: 'currentScore', headerName: 'CurrentScore', width: '25%' }
+  ];
+
+  const options = {
+    onRowClick: rowData => console.log(rowData)
+  };
+
+  useEffect(() => {
+    let url = 'https://interviewprepapp.azurewebsites.net/api/quiz/20';
+    fetch(url, {
     method: "GET",
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': "*",
     }
-  }).then(res => res.json())
-  .then(quizzes => {
-    return quizzes;
-  })
-}
-
-const Quizzes = () =>{
-  const [selectedQuiz, selectQuiz] = React.useState();
-  const [quizzes, setQuizzes] = React.useState();
-
-  const columns = [
-    { field: 'date', headerName: 'ID', width: '35%' },
-    { field: 'userName', headerName: 'Username', width: '35%' },
-    { field: 'currentScore', headerName: 'Current Score', width: '30%' },
-  ];
-  const options = {
-    onRowClick: rowData => console.log(rowData)
-  };
-  React.useEffect(() => {
-    setQuizzes(getQuizzes());
+    }).then(res => {
+      console.log(res);
+      return res.json()
+    })
+    .then(quizzes => {
+      // console.log(quizzes);
+      setQuizzes(quizzes);
+      setRows(quizzes.map(quiz => ({ id: quiz.id, date: quiz.createdDate, username: quiz.username ? quiz.username : 'unknown', currentScore: (quiz.upvotes + quiz.downvotes) ? `${quiz.upvotes/(quiz.upvotes + quiz.downvotes)}%` : 0 })));
+    })
   }, []);
-  const rows = quizzes.map(quiz => ({ date: quiz.createdDate, username: quiz.username, currentScore: `${quiz.upvotes/(quiz.upvotes + quiz.downvotes)}%` }));
+  
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      {/* {selectedQuiz ?  */}
-      <DataGrid rows={rows} columns={columns} pageSize={rows.length > 10 ? 10 : rows.length} options={options} />
+    <div style={{ height: 400, width: '100%', backgroundColor: 'white' }}>
+    {console.log(columns)}  
+    {console.log(rows)}  
+
+    {/* {selectedQuiz ?  */}
+      {rows ? 
+        <DataGrid rows={rows} columns={columns} pageSize={rows.length > 10 ? 10 : rows.length}/> :
+        <>
+        </>
+      }
         {/* : viewQuiz()} */}
     </div>
   );
